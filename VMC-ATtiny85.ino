ac2111e64e5950bbf6de2f744716190911f257dd
Maxence Abela <maxence.abela@epitech.eu>
2021-01-25 22:08:38 +0100

    fix error handling for invalid measures.

--- VMC-ATtiny85.ino
+++ VMC-ATtiny85.ino
@@ -16,8 +16,7 @@
 #define RELAY_PIN 1
 #define LED_PIN 3
 #define DHT11_PIN 4
-#define REGULAR_HUMIDITY 64.0
-#define CEILING_HUMIDITY 80 //Used in case we suddenly have a big reading, because we don't want to trigger the relay.
+#define REGULAR_HUMIDITY 55.0
 
 #define RELAY_ON() digitalWrite(RELAY_PIN, LOW)
 #define RELAY_OFF() digitalWrite(RELAY_PIN, HIGH)
@@ -25,6 +24,7 @@
 #define LED_OFF() digitalWrite(LED_PIN, LOW)
 
 dht DHT;
+int latest_measure;
 
 void flash_led(int measure)
 {
@@ -53,15 +53,22 @@ int check_measure(int check)
   return 0;
 }
 
+int impossible_variation(int measure)
+{
+  if (abs(measure - latest_measure) > 30)
+    return 1;
+  return 0;
+}
+
 void led_error_display(void)
 {
-    LED_ON();
-    delay(50);
-    LED_OFF();
-    delay(50);
-    LED_ON();
-    delay(50);
-    LED_OFF();
+  LED_ON();
+  delay(50);
+  LED_OFF();
+  delay(50);
+  LED_ON();
+  delay(50);
+  LED_OFF();
 }
 
 void setup()
@@ -71,9 +78,16 @@ void setup()
   LED_OFF();
   RELAY_ON();
   LED_ON();
-  delay(2000);
+  delay(5000);
   RELAY_OFF();
   LED_OFF();
+  int chk = DHT.read11(DHT11_PIN);
+  if (check_measure(chk)) {
+    led_error_display();
+    RELAY_OFF();
+    return;
+  }
+  latest_measure = DHT.humidity;
 }
 
 void loop()
@@ -81,7 +95,7 @@ void loop()
   int chk;
   int h = 0;
   
-  delay(3000);
+  delay(2500);
   chk = DHT.read11(DHT11_PIN);
   if (check_measure(chk)) {
     led_error_display();
@@ -89,11 +103,15 @@ void loop()
     return;
   }
   h = DHT.humidity;
+  if (impossible_variation(h)) {
+    return;
+  }
+  latest_measure = h;
   flash_led(h - 5);
   if (h < REGULAR_HUMIDITY) {
     LED_OFF();
     RELAY_OFF();
-  } else  if (h > REGULAR_HUMIDITY + 3.0 && h < CEILING_HUMIDITY) {
+  } else  if (h > REGULAR_HUMIDITY + 5.0) {
     LED_ON();
     RELAY_ON();
   }
